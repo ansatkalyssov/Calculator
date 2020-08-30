9b93b3eade171665b2523887e1b43bb5672fbc1c
ansat <ansat.kalysov@gmail.com>
2020-08-30 17:17:38 +0600

    Importing Project

---
+++ Calculator/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Windows;
+
+namespace Calculator.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Calculator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.ViewModels
+{
+    class MainWindowViewModel : BaseViewModel
+    {
+
+        private string _synchronizedText;
+        public string SynchronizedText
+        {
+            get => _synchronizedText;
+            set
+            {
+                _synchronizedText = value;
+                OnPropertyChanged(nameof(SynchronizedText));
+            }
+        }
+    }
+}
